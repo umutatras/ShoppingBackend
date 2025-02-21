8685a82dfe0a0893c29e67ed0075bb3ca68c3821
umutatras <umutatras@gmail.com>
2025-02-21 21:17:35 +0300

    CurrentUserManager servisi yazıldı ve appsettingjson jwt config ayarları yapıldı.

---
+++ ShoppingBackend.WebAPI/Services/CurrentUserManager.cs
@@ -0,0 +1,30 @@
+﻿using ShoppingBackend.Application.Common.Interfaces;
+using System.Security.Claims;
+
+namespace ShoppingBackend.WebAPI.Services;
+
+public class CurrentUserManager : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IWebHostEnvironment _env;
+    public CurrentUserManager(IHttpContextAccessor contextAccessor, IWebHostEnvironment webHostEnvironment)
+    {
+        _httpContextAccessor = contextAccessor;
+        _env = webHostEnvironment;
+    }
+
+    public Guid UserId => GetUserId();
+
+
+    private Guid GetUserId()
+    {
+        var userId = _httpContextAccessor
+            .HttpContext?
+            .User?
+            .FindFirstValue("uid");
+
+        return string.IsNullOrEmpty(userId) ? Guid.Empty : Guid.Parse(userId);
+    }
+
+
+}
