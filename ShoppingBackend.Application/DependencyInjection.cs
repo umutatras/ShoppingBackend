fcdf256eeb26192e23f72e98b0e04339ced70e7d
umutatras <umutatras@gmail.com>
2025-02-21 21:19:30 +0300

    Application DependencyInjection class ve validation davranış biçimi oluşturuldu.

---
+++ ShoppingBackend.Application/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingBackend.Application.Common.Behaviours;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBackend.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicaton(this IServiceCollection services)
+    {
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                config.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviours<,>));
+            });
+            return services;
+        }
+    }
+}
